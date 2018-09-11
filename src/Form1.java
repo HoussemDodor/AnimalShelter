825ee178b53454a2ce141ebf96f0c6d8f8038692
Houssem <Houssem@dodor.nl>
2018-09-11 11:42:55 +0200

    GUI Form added with buttons

---
+++ src/Form1.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Form1 {
+    private JComboBox comboBox_Species;
+    private JPanel panel_Main;
+    private JLabel lbl_species;
+    private JLabel lbl_name;
+    private JRadioButton radioBttn_Female;
+    private JRadioButton radioBttn_Male;
+    private JLabel lbl_gender;
+    private JTextField txtBox_Name;
+    private JTextField txtBox_BadHabits;
+    private JLabel lbl_AnimalsList;
+    private JList listbox_Animal;
+    private JLabel lbl_Reserve;
+    private JLabel lbl_ReservorName;
+    private JTextField txtBox_reservorName;
+    private JButton bttn_Reserve;
+    private JButton bttn_AddAnimal;
+}

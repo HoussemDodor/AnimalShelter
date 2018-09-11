b221ed4332453ae27c3f9e5d9da3336052a1b397
Houssem <Houssem@dodor.nl>
2018-09-11 10:35:18 +0200

    Cat Class Afgerond

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -12,36 +12,24 @@ public abstract class Animal {
         return Name;
     }
 
-    public void setName(String name) {
-        Name = name;
-    }
-
     public Animals.Gender getGender() {
         return Gender;
-    }
-
-    public void setGender(Animals.Gender gender) {
-        Gender = gender;
     }
 
     public Reservor getReservedBy() {
         return ReservedBy;
     }
 
-    public void setReservedBy(Reservor reservedBy) {
-        ReservedBy = reservedBy;
-    }
-
     // Constructor
     public Animal(String name, Animals.Gender gender) {
-        setName(name);
-        setGender(gender);
+        this.Name = name;
+        this.Gender = gender;
     }
 
     // Methods
     public boolean Reserve(String reservedBy) {
         if (getReservedBy() == null) {
-            this.setReservedBy(new Reservor(reservedBy, LocalDateTime.now()));
+            this.ReservedBy = new Reservor(reservedBy, LocalDateTime.now());
             return true;
         }
         return  false;
@@ -53,6 +41,6 @@ public abstract class Animal {
         {
             reserved = String.format("reserved by %d", getReservedBy().getName());
         }
-        return String.format("%d, %d, $d", this.getName(), this.getGender(), reserved);
+        return String.format("%d, %d, %d", this.getName(), this.getGender(), reserved);
     }
 }
--- src/Animals/Cat.java
+++ src/Animals/Cat.java
@@ -1,4 +1,24 @@
 package Animals;
 
-public class Cat {
+public class Cat extends Animal{
+
+    private String BadHabits;
+
+    public String getBadHabits() {
+        return BadHabits;
+    }
+
+    public void setBadHabits(String badHabits) {
+        BadHabits = badHabits;
+    }
+
+    public Cat(String name, Gender gender, String badHabits){
+        super(name, gender);
+        this.BadHabits = badHabits;
+    }
+
+    public String ToString() {
+        return super.ToString() + String.format("bad habits: %d", getBadHabits().toLowerCase());
+    }
+
 }
